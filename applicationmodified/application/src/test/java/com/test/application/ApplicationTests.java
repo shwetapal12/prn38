10f32a49b5e2deb7c1521b6c9f1f63e57b0d0024
group004 <shwetapal9158@gmail.com>
2019-06-15 11:35:21 +0530

    This is my modified testNG project

--- applicationmodified/application/src/test/java/com/test/application/ApplicationTests.java
+++ applicationmodified/application/src/test/java/com/test/application/ApplicationTests.java
@@ -1,16 +1,18 @@
-package com.exam.application;
-
-import org.junit.Test;
+package com.test.application;
+import org.testng.annotations.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
 
+	
 	@Test
 	public void contextLoads() {
+		System.out.println("------This is my Test NG application-------------");
 	}
 
 }
